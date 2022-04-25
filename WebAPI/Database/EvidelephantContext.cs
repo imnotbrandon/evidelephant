1c9d74d2b38ce54a2f17f6c962a2d8418e479065
JuzTim <wuestenhagen.tim@gmail.com>
2022-04-25 19:00:45 +0200

    Add basic db model

---
+++ WebAPI/Database/EvidelephantContext.cs
@@ -0,0 +1,13 @@
+﻿using Evidelephant.WebAPI.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Evidelephant.WebAPI.Database;
+
+public class EvidelephantContext : DbContext
+{
+    public EvidelephantContext(DbContextOptions<EvidelephantContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Evidence> Evidence { get; set; }
+}
---
+++ WebAPI/Database/Models/Evidence.cs
@@ -0,0 +1,7 @@
+﻿namespace Evidelephant.WebAPI.Database.Models;
+
+public class Evidence
+{
+    public Guid Id { get; set; }
+    public DateTime Created { get; set; }
+}
